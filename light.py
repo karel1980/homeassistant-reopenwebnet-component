57bdd63c82fded236f629d2a422277e7e341a662
Karel Vervaeke <karel@vervaeke.info>
2019-04-07 15:21:35 +0200

    Remember brightness when toggling on/off

--- light.py
+++ light.py
@@ -33,6 +33,7 @@ class MyHomeLight(Light):
         self._name = light[CONF_NAME]
         self._address = light['address']
         self._state = False
+        self._brightness = 0
 
     @asyncio.coroutine
     def async_added_toHass(self):
@@ -46,9 +47,31 @@ class MyHomeLight(Light):
     def is_on(self):
         return self._state
 
+    @property
+    def brightness(self):
+        return self._brightness
+
     def turn_on(self, **kwargs):
-        self._gate.light_on(self._address)
-        self._state = True
+        brightness = kwargs.get('brightness', None)
+        if brightness is None:
+            self._gate.light_command(self._address, self._brightness)
+            self._state = True
+        elif brightness == 0:
+            self._gate.light_off(self._address)
+            self._state = False
+            self._brightness = 0
+        elif brightness <= 2:
+            self._state = True
+            self._brightness = 2
+            self._gate.light_command(self._address, '2')
+        elif brightness <= 10:
+            self._state = True
+            self._brightness = brightness 
+            self._gate.light_command(self._address, str(brightness))
+        else:
+            self._state = True
+            self._brightness = 10
+            self._gate.light_command(self._address, str(brightness))
 
     def turn_off(self, **kwargs):
         self._gate.light_off(self._address)
@@ -56,6 +79,21 @@ class MyHomeLight(Light):
 
     def update(self):
         state = self._gate.light_status(self._address)
-        self._state = state
+        if state is None:
+            self._state = False
+        if state == '0':
+            self._state = False
+        elif state == '1':
+            self._state = True
+            self._brightness = 10
+        elif int(state) <= 2:
+            self._state = True
+            self._brightness = 2
+        elif int(state) <= 10:
+            self._state = True
+            self._brightness = int(state) 
+        else:
+            self._state = True
+            self._brightness = 10
 
         self.schedule_update_ha_state()
